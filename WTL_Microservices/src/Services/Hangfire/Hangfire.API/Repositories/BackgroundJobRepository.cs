f27455909fad7dbf87c460551f52c1ccef1e9501
PhatNT <phatnt0302@gmail.com>
2024-03-03 21:46:10 +0700

    feat: config, setup email smtp

--- WTL_Microservices/src/Services/Hangfire/Hangfire.API/Repositories/BackgroundJobRepository.cs
+++ WTL_Microservices/src/Services/Hangfire/Hangfire.API/Repositories/BackgroundJobRepository.cs
@@ -1,9 +1,48 @@
-﻿using Hangfire.API.Repositories.Interfaces;
+﻿using Contracts.Services;
+using Hangfire.API.Repositories.Interfaces;
+using Shared.Common.Interfaces;
+using Shared.Services.Email;
+using ILogger = Serilog.ILogger;
 
 namespace Hangfire.API.Repositories
 {
     public class BackgroundJobRepository: IBackgroundJobRepository
     {
+        private readonly IScheduledJobService _jobService;
+        private readonly ISmtpEmailService _emailService;
+        private readonly ILogger _logger;
 
+        public BackgroundJobRepository(IScheduledJobService jobService, ISmtpEmailService emailService, ILogger logger)
+        {
+            _jobService = jobService;
+            _emailService = emailService;
+            _logger = logger;
+        }
+
+        public IScheduledJobService ScheduledJobService => _jobService;
+
+        public string SendEmailContent(string email, string subject, string emailContent, DateTimeOffset enqueueAt)
+        {
+            var emailRequest = new MailRequest
+            {
+                ToAddress = email,
+                Body = emailContent,
+                Subject = subject
+            };
+
+            try
+            {
+                var jobId = _jobService.Schedule(() => _emailService.SendEmail(emailRequest), enqueueAt);
+                _logger.Information($"Sent email to {email} with subject: {subject} - Job Id: {jobId}");
+
+                return jobId;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"failed due to an error with the email service: {ex.Message}");
+            }
+
+            return null;
+        }
     }
 }
